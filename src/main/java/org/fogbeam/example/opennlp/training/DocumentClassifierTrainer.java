f1330a59af447a5281db6a9bab4e2129f486bf28
eglezgut03 <alu0101461588@ull.edu.es>
2024-11-20 09:13:43 +0000

    Corrección error catch SonarCloud

--- src/main/java/org/fogbeam/example/opennlp/training/DocumentClassifierTrainer.java
+++ src/main/java/org/fogbeam/example/opennlp/training/DocumentClassifierTrainer.java
@@ -18,6 +18,9 @@ import opennlp.tools.util.PlainTextByLineStream;
 
 
 public class DocumentClassifierTrainer {
+
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(DocumentClassifierTrainer.class.getName());
+
   public static void main( String[] args ) throws Exception {
     DoccatModel model = null;
     InputStream dataIn = null;
@@ -29,20 +32,16 @@ public class DocumentClassifierTrainer {
               lineStream );
       model = DocumentCategorizerME.train( "en", sampleStream );
     }
-    catch( IOException e ) {
-      // Failed to read or parse training data, training failed
-      e.printStackTrace();
+    catch (Exception e) {
+      logger.severe("Error" + e.getMessage());
     }
     finally {
       if( dataIn != null ) {
         try {
           dataIn.close();
         }
-        catch( IOException e ) {
-          // Not an issue, training already finished.
-          // The exception should be logged and investigated
-          // if part of a production system.
-          e.printStackTrace();
+        catch (Exception e) {
+          logger.severe("Error" + e.getMessage());
         }
       }
     }
@@ -53,19 +52,16 @@ public class DocumentClassifierTrainer {
               modelFile ) );
       model.serialize( modelOut );
     }
-    catch( IOException e ) {
-      // Failed to save model
-      e.printStackTrace();
+    catch (Exception e) {
+      logger.severe("Error" + e.getMessage());
     }
     finally {
       if( modelOut != null ) {
         try {
           modelOut.close();
         }
-        catch( IOException e ) {
-          // Failed to correctly save model.
-          // Written model might be invalid.
-          e.printStackTrace();
+        catch (Exception e) {
+          logger.severe("Error" + e.getMessage());
         }
       }
     }
